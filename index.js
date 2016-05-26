0a8df4aa4b90748f85c87e505f07d32dc460b132
joyqi <magike.net@gmail.com>
2016-05-26 12:59:21 +0800

    add udp socket

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 
 var Opt = require('optimist'),
-    Net = require('net');
+    Net = require('net'),
+    Udp = require('dgram');
 
 
 var argv = Opt
-    .usage('Usage: $0 [ -p 80 ] [ -t 10.0.10.11:80 ]')
-    .demand(['p', 't'])
+    .usage('Usage: $0 [ -l 80 ] [ -r 10.0.10.11:80 ] [ -t udp ]')
+    .demand(['l', 'r'])
     .boolean('h')
+    .alias('t', 'type')
     .alias('h', 'help')
-    .describe('p', 'Local port.')
-    .describe('t', 'Remote address.')
+    .alias('l', 'local')
+    .alias('r', 'remote')
+    .default('t', 'tcp')
+    .describe('l', 'Local address.')
+    .describe('r', 'Remote address.')
+    .describe('t', 'Socket type.')
     .argv;
 
 if (argv.h) {
@@ -17,29 +23,96 @@ if (argv.h) {
     process.exit(0);
 }
 
-var localParsed = (argv.p + '').split(':'),
-    targetParsed = (argv.t + '').split(':'),
-    localPort = localParsed[localParsed.length - 1],
-    localHost = localParsed.length > 1 ? localParsed[0] : '0.0.0.0',
-    targetHost = targetParsed[0],
-    targetPort = targetParsed.length > 1 ? targetParsed[1] : argv.p;
+// parse and validate port number
+function parsePort(port) {
+    if ((port + '').match(/^[0-9]+$/i)) {
+        return parseInt(port);
+    }
 
-var server = Net.Server(function (client) {
-    var socket = Net.connect(targetPort, targetHost);
-    client.pipe(socket).pipe(client);
+    console.log(port + ' is not a valid port number.');
+    process.exit(1);
+}
+
+// parse and validate ip address
+function parseAddress(address) {
+    var parsed = (address + '').split(':'),
+        result = {};
+
+    if (parsed.length > 1) {
+        isIP = Net.isIP(parsed[0]);
+
+        if (!isIP) {
+            console.log(parsed[0] + ' is not a valid ip address.');
+            process.exit(1);
+        }
+
+        result = {ip : parsed[0], port : parsePort(parsed[1]), type : isIP};
+    } else {
+        result = {ip : '0.0.0.0', port : parsePort(parsed[0]), type : 4}
+    }
+
+    return result;
+}
+
+var localAddress = parseAddress(argv.l),
+    remoteAddress = parseAddress(argv.r);
+
+if (argv.t == 'tcp') {
+    var server = Net.Server(function (client) {
+        var socket = Net.connect(remoteAddress.port, remoteAddress.ip);
+        client.pipe(socket).pipe(client);
     
-    console.log("Connecting " + client.remoteAddress 
-        + ":" + client.remotePort);
+        console.log("Request " + client.remoteAddress 
+            + ":" + client.remotePort);
 
-    client.on('error', function (err) {
+        client.on('error', function (err) {
+            console.log("Error " + err);
+        });
+    });
+
+    server.listen(localAddress.port, localAddress.ip);
+
+    server.on('error', function (err) {
         console.log("Error " + err);
     });
-});
+} else {
+    var receiver = Udp.createSocket('udp' + localAddress.type),
+        sender = Udp.createSocket('udp' + remoteAddress.type),
+        client = null;
 
-server.listen(localPort, localHost);
-console.log("Listening at " + argv.p);
+    receiver.bind(localAddress.port, localAddress.ip);
+    receiver.ref();
+    sender.bind();
+    sender.ref();
 
-server.on('error', function (err) {
-    console.log("Error " + err);
-});
+    receiver.on('error', function (err) {
+        console.log("Error " + err);
+    });
+    
+    sender.on('error', function (err) {
+        console.log("Error " + err);
+    });
+
+    receiver.on('message', function (data, info) {
+        client = info;
+
+        console.log("Request " + client.address
+            + ":" + client.port);
+
+        sender.send(data, 0, data.length, remoteAddress.port, remoteAddress.ip);
+    });
+
+    sender.on('message', function (data, info) {
+        console.log("Response " + info.address
+            + ":" + info.port);
+
+        if (client) {
+            receiver.send(data, 0, data.length, client.port, client.address);
+        }
+    });
+}
+
+
+console.log("Piping " + localAddress.ip + ":" + localAddress.port 
+    + " to " + remoteAddress.ip + ":" + remoteAddress.port + " via " + argv.t);
 
